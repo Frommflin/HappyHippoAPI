f99b5f44a1a3736f63b37aa281612b72a44bd27d
Frommflin <freakyfrommelin@outlook.com>
2024-02-11 13:00:28 +0100

    Modifying cors origins

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 });
 builder.Services.AddCors(options => options.AddPolicy(name: "HappyHippo", policy =>
 {
-   policy.WithOrigins("https://happyhippo-jt5w.onrender.com").AllowAnyMethod().AllowAnyHeader();
+   policy.WithOrigins("https://happyhippo-jt5w.onrender.com/*").AllowAnyMethod().AllowAnyHeader();
 }));
 
 var app = builder.Build();
